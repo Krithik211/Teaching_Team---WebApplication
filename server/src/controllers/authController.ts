eaf62751b12073dbfa7800d321a7976861319f37
NihilRama <s4001201@student.rmit.edu.au>
2025-06-01 23:15:00 +1000

    updated

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../data-source";
-import { User } from "../entities/User";
+import { User } from "../entities/user";
 import bcrypt from "bcryptjs";
 
 export const loginUser= async (req: Request, res: Response): Promise<any> => {
